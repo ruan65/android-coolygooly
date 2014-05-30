a3d2abc822195f61312133ff12e6896462fc0959
ruan65 <ruan65@gmail.com>
2014-05-29 20:20:21 -0700

    short a little bit

--- NotFinalCountDown/app/src/main/java/com/ruan65/notfinalcountdown/app/CountDown.java
+++ NotFinalCountDown/app/src/main/java/com/ruan65/notfinalcountdown/app/CountDown.java
@@ -1,11 +1,8 @@
 package com.ruan65.notfinalcountdown.app;
 
-
+import android.os.Bundle;
 import android.os.Looper;
 import android.support.v7.app.ActionBarActivity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -20,7 +17,6 @@ public class CountDown extends ActionBarActivity implements View.OnClickListener
     private long startTime = 20_000;
     private final long interval = 2_000;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,27 +29,6 @@ public class CountDown extends ActionBarActivity implements View.OnClickListener
 
         tv.setText(tv.getText() + String.valueOf(startTime / 1000));
 
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.count_down, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            startTime = 60_000;
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
     }
 
     public void onClick(View view) {
@@ -93,7 +68,6 @@ public class CountDown extends ActionBarActivity implements View.OnClickListener
             tv.setText("" + l / 1000);
         }
 
-        @Override
         public void onFinish() {
             tv.setText("Time's up!");
         }
